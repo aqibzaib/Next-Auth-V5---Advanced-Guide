d09cf9440929b2d93c7d89b3f26cfb2dcff68366
Aqib <aqib@Aqibs-MacBook-Pro.local>
2025-07-26 18:26:10 +0500

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["600"],
+  subsets: ["latin"],
+});
 
 export default function Home() {
-  return <main className="flex h-full flex-col justify-center items-center bg-sky-500 border "></main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_#38bdf8,_#1e3a8a)]">
+      <div className={` space-y-6 text-center`}>
+        <h1
+          className={`${poppins.className} text-6xl font-semibold text-white drop-shadow-md`}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+
+        <Button variant="secondary" size={"lg"}>
+          Sign In
+        </Button>
+      </div>
+    </main>
+  );
 }

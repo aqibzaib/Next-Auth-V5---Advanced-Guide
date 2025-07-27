fc07ebad2aa70cb2a61bd82add387586a96950d8
Aqib <chaqib4u@gmail.com>
2025-07-28 01:09:46 +0500

    feat: add CardWrapper and Header components for login form UI

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { CardWrapper } from "./card-wrapper";
 
 export default function LoginForm() {
-  return <div>Login Form</div>;
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      Login Form!
+    </CardWrapper>
+  );
 }

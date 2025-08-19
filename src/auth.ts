46b24628701ce99dd0e21787bb2bff1a050ed86f
Aqib <chaqib4u@gmail.com>
2025-08-19 22:13:04 +0500

    feat: integrate Prisma adapter and JWT session strategy in NextAuth configuration

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import authConfig from "./auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });

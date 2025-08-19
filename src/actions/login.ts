8bb2a1060d8035ebcf3fae13a948060775d1999b
Aqib <chaqib4u@gmail.com>
2025-08-20 01:47:50 +0500

    working auth with email and password

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -18,6 +18,8 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       password,
       redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
+
+    return { success: "Logged in successfully!" };
   } catch (err) {
     if (err instanceof AuthError) {
       switch (err.type) {

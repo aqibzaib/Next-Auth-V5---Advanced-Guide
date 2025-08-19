fb8d03f1907ba139bc521ebddf02564a1bed38cd
Aqib <chaqib4u@gmail.com>
2025-08-20 01:58:46 +0500

    feat: add SignOut action and implement signout functionality in Setting page

--- src/app/(protected)/setting/page.tsx
+++ src/app/(protected)/setting/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { SignOut } from "@/actions/signout";
 export default async function Setting() {
   const session = await auth();
   return (
     <div className="flex justify-center items-center flex-col">
       <h1>Setting Page</h1>
       <h2>Session: {JSON.stringify(session)}</h2>
+
+      <form action={SignOut}>
+        <Button type="submit">Signout</Button>
+      </form>
     </div>
   );
 }
